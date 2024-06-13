85a9ad4c6c8dbd52e57707722927b65fe1d055c1
Shakhnazar Sailaukan <101112128+Sailaukan@users.noreply.github.com>
2024-06-13 09:58:32 +0500

    my commit

--- src/events/event-router.ts
+++ src/events/event-router.ts
@@ -11,5 +11,4 @@ eventRouter.get('/events/', eventController.getEvents);
 eventRouter.post('/events/', eventController.createEvent);
 eventRouter.get('/events/:id', eventController.getEventById);
 eventRouter.get('/find-events/', eventController.getCityByToken);
-
 export default eventRouter;
